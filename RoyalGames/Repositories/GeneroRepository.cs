c5cecedb498456eb2d85ab91e39d61fed8dfcd7b
mariana <mariiiihcordeiro@gmail.com>
2026-03-04 06:37:40 -0300

    :sparkles:feat(genero): criação da interface IGeneroRepository e estrutura inicial do GeneroRepository

---
+++ RoyalGames/Repositories/GeneroRepository.cs
@@ -0,0 +1,92 @@
+﻿using RoyalGames.Contexts;
+using RoyalGames.Domains;
+using RoyalGames.Interfaces;
+
+namespace RoyalGames.Repositories
+{
+    public class GeneroRepository : IGeneroRepository
+    {
+        private readonly RoyalGamesContext _context;
+
+        public GeneroRepository(RoyalGamesContext context)
+        {
+            _context = context;
+        }
+
+        public List<Genero> Listar()
+        {
+            return _context.Genero.ToList();
+        }
+
+        // Busca uma genero pelo ID.
+        public Genero ObterPorId(int id)
+        {
+            // Procura no banco o primeiro genero com o ID informado.
+            Genero genero  = _context.Genero.FirstOrDefault(g => g.GeneroID == id);
+
+            // Retorna o genero encontrada ou null se não existir.
+            return genero;
+        }
+
+        // Verifica se já existe um genero com o mesmo nome.
+        // generoIdAtual é usado quando se está editando.
+        public bool NomeExiste(string nome, int? GeneroIdAtual = null)
+        {
+            // AsQueryable() => Consulta a tabela plataforma sem executar nada no banco.
+            var consulta = _context.Genero.AsQueryable();
+
+            // Se o ID atual for informado, signifca que o genero existente está sendo editada.
+            // O próprio genero será ignorada na verificação.
+
+            if (GeneroIdAtual.HasValue)
+            {
+                // Remove da busca o próprio genero
+                // Evita duplicidade.
+                consulta = consulta.Where(g => g.GeneroID != GeneroIdAtual.Value);
+            }
+
+            //Verificação de genero com o mesmo nome, caso encontre retorna true se não false
+            return consulta.Any(g => g.Nome == nome);
+        }
+
+        // Adiciona um novo genero no banco.
+        void Adicionar(Genero genero)
+        {
+            _context.Plataforma.Add(genero);
+            _context.SaveChanges();
+        }
+
+        void Atualizar(Genero genero)
+        {
+            // Busca um genero pelo ID no banco 
+            Genero generoBanco = _context.Genero.FirstOrDefault(g => g.GeneroID == genero.GeneroID);
+
+            // Se não encontrar nada retorna a saído do método.
+            if (generoBanco == null)
+            {
+                return;
+            }
+
+            // Atualização do campo nome
+            generoBanco.Nome = genero.Nome;
+            _context.SaveChanges();
+        }
+
+        // Remove um genero pelo ID
+        void Remover(int id)
+        {
+            // Busca um genero pelo ID no banco 
+            Genero generoBanco = _context.Genero.FirstOrDefault(g => g.GeneroID == id);
+
+            // Se não encontrar nada retorna a saído do método.
+            if (generoBanco == null)
+            {
+                return;
+            }
+
+            // Remoção do genero
+            _context.Genero.Remove(generoBanco);
+            _context.SaveChanges();
+        }
+    }
+}
